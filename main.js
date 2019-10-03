dc93905a32ca2b7829c9e9a876c8e3d3f6cd5d55
DonPascualino <pascalr.92@gmail.com>
2019-10-03 11:19:59 +0200

    func

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const msg = document.querySelector('#msg');
-const wakeup = document.querySelector('#wakeuptime');
+const wakeup = document.querySelector('#wakeup');
 const sleeptime = document.querySelector('#sleeptime');
 // const timetofallasleep = ???
 
-document.addEventListener("submit", zzz);
 
-function zzz(wakeup, sleeptime) {
-  const zz = wakeup.value - sleeptime.value;
-  console.log(zz);
+console.log(wakeup.value);
+console.log(sleeptime.value);
+
+function diff(start, end) {
+  start = start.split(":");
+  end = end.split(":");
+  var startDate = new Date(0,0,0, start[0], start[1], 0);
+  var endDate = new Date(0,0,0, start[0], start[1], 0);
 }
